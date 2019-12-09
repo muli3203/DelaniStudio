bfbb3209ab1f161f43c7faf2b90e73d029a10899
muli c <muli3203@gmail.com>
2019-12-09 13:12:19 +0300

    modify the paragraphs in the 'about us' section.

--- js/index.js
+++ js/index.js
@@ -7,4 +7,20 @@ $(document).ready(function() {
         $(".design-showing").toggle();
         $(".design-hidden").toggle();    
     })
+    $(".dev-hidden").click(function() {
+        $(".dev-showing").toggle();
+        $(".dev-hidden").toggle();    
+    });
+    $(".dev-showing").click(function() {
+        $(".dev-showing").toggle();
+        $(".dev-hidden").toggle();    
+    })
+    $(".prod-hidden").click(function() {
+        $(".prod-showing").toggle();
+        $(".prod-hidden").toggle();    
+    });
+    $(".prod-showing").click(function() {
+        $(".prod-showing").toggle();
+        $(".prod-hidden").toggle();    
+    })
 })
